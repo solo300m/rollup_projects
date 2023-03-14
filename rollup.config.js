bf4544f30024e6b84e0d605ebddc38369609ca6f
Sergey Lyulchenko <solo300m@yandex.ru>
2023-03-14 20:20:21 +0400

    задание 2

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,9 @@
 import { babel } from '@rollup/plugin-babel';
+import styles from 'rollup-plugin-styles';
+import image from 'rollup-plugin-img';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
 
 export default {
     input: './src/index.js',
@@ -8,9 +13,15 @@ export default {
     },
     plugins: [
 		babel({ babelHelpers: 'bundled' }),
-		/*styles(),
+		styles(),
 		image({
-			limit: 1000000,
-		  }),*/
+			limit: 100000,
+		  }),
+    serve({
+      open: true,
+      contentBase: './',
+      port:8000,
+    }),
+    livereload(),
     ]
   };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import "../css/index.css";
+import pick from '../assets/odinokij-volk23.jpg';
+
+
 console.log('Hello, World!');
+const imgW = document.createElement('div');
+//imgW.classList.add('img-style');
+const pickche = document.createElement('img');
+pickche.classList.add('pick_style');
+pickche.src = pick;
+imgW.append(pickche);
+
+const bodyHTML = document.querySelector('body');
+
+bodyHTML.append(imgW);
